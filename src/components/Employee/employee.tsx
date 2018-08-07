3e7d32d6709c34c789bbbeddb773e4afecafa4dd
John Wilson <irewilson@gmail.com>
2018-08-07 10:35:28 +0800

    Update employee.tsx

--- src/components/Employee/employee.tsx
+++ src/components/Employee/employee.tsx
@@ -4,10 +4,9 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
 //
-import TextBoxComponent from "../elements/TextBox";
+import { Employee } from "../../models/employee";
+import TextBoxComponent from "../elements/textBox";
 import * as employeeActions from "../../actions/employeeActions";
-
-import { Employee } from "../../model/employee";
 
 interface IEmployeeComponentProps {
   employeeDetails: any;
@@ -16,32 +15,44 @@ interface IEmployeeComponentProps {
   actions: any;
   result: any;
   response: any;
+  isUpdate: boolean
 }
 
 interface IEmployeeComponentState {
   employee: Employee;
+  isUpdate?: boolean;
 }
 
-class EmployeeComponent extends React.Component<
-  IEmployeeComponentProps,
-  IEmployeeComponentState
-> {
+class EmployeeComponent extends React.Component<IEmployeeComponentProps, IEmployeeComponentState> {
   constructor(props: any) {
     super(props);
 
     this.state = {
-      employee: new Employee()
+      employee: new Employee(),
+      isUpdate: false
     };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    this.props.actions.getEmployeeData(this.props.match.params.id).then(() => {
-      console.log('my callbvack', this.props.employeeDetails)
-        
-    });
+    let isAdd = this.props.match.params.id === undefined ? false : true;   
 
+    if (isAdd) {
+      this.props.actions.getEmployeeData(this.props.match.params.id).then(() => {
+          let employeeObject = this.props.employeeDetails.data[0];
+          this.setState({ 
+            isUpdate: true,
+            employee: employeeObject
+           });
+        });
+    }
+  }
+
+  componentWillReceiveProps(nextProps: any) {
+    //let employeeObject = nextProps.employeeDetails.data[0];
+    //console.log("next props", employeeObject);
+    //this.setState({employee : employeeObject })
   }
 
   componentDidUpdate(prevProps: any) {
@@ -53,9 +64,9 @@ class EmployeeComponent extends React.Component<
 
   render() {
     return (
-      <div className="add-container">
-        <h2 className="header-wrapper">Add Employee</h2>
-        <div className="l-container">
+      <div className="employee-container">
+        <h2 className="header-wrapper">Employee Details</h2>
+        <div className="employee-wrapper">
           <div className="l-wrapper">
             <TextBoxComponent
               label="Name"
@@ -63,7 +74,7 @@ class EmployeeComponent extends React.Component<
               type="text"
               focus={true}
               placeholder="Enter Name"
-              value={this.state.employee.name}
+              value={this.state.employee.name || ''}
               onChange={this.handleChange}
             />
 
@@ -72,7 +83,7 @@ class EmployeeComponent extends React.Component<
               name="userName"
               type="text"
               placeholder="Enter Username"
-              value={this.state.employee.userName}
+              value={this.state.employee.userName || ''} 
               onChange={this.handleChange}
             />
             <TextBoxComponent
@@ -80,7 +91,7 @@ class EmployeeComponent extends React.Component<
               name="email"
               type="text"
               placeholder="Enter Email"
-              value={this.state.employee.email}
+              value={this.state.employee.email || ''}
               onChange={this.handleChange}
             />
 
@@ -89,7 +100,7 @@ class EmployeeComponent extends React.Component<
               name="phone"
               type="text"
               placeholder="Enter Phone"
-              value={this.state.employee.phone}
+              value={this.state.employee.phone || ''}
               onChange={this.handleChange}
             />
 
@@ -98,11 +109,16 @@ class EmployeeComponent extends React.Component<
               name="website"
               type="text"
               placeholder="Enter Website"
-              value={this.state.employee.website}
+              value={this.state.employee.website || ''}
               onChange={this.handleChange}
             />
 
-            <button className="btn-save">Save</button>
+            {
+              (this.state.isUpdate == true) ?  <button className="btn-save">Update</button> : <button className="btn-add">Add</button>
+            
+            }
+      
+          
           </div>
         </div>
       </div>
@@ -122,7 +138,4 @@ const mapDispatchToProps = (dispatch: any) => {
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(EmployeeComponent);
+export default connect( mapStateToProps, mapDispatchToProps)(EmployeeComponent);
