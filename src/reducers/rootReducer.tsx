2255a5b3efd4ce41ba07c97fe5d66c4a46439154
John Wilson <irewilson@gmail.com>
2018-08-07 18:36:58 +0800

    Update rootReducer.tsx

--- src/reducers/rootReducer.tsx
+++ src/reducers/rootReducer.tsx
@@ -1,8 +1,19 @@
 
-import { combineReducers } from 'redux'
-import { employeeReducer } from './Employee/employeeReducer';
+import { combineReducers } from 'redux';
 
-export const rootReducer = combineReducers({
-  employees: employeeReducer,
+import error from './error/errorReducer';
+import isLoading from './loading/loadingReducer';
+import employees from './employeesList/employeesListReducer';
+import employee from './employeeDetails/employeeDetailsReducer'
+
+
+const rootReducer = combineReducers({
+  error,
+  isLoading,
+  employees,
+  employee,
 })
+
+export default rootReducer;
+
 
