d9b46792c91e3e8ebd7c4d186be6175c271e33ca
John Wilson <irewilson@gmail.com>
2018-08-07 10:36:21 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,26 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
-import { configureStore } from './stores/configureStore';
+import { configureStore } from '../store/configureStore';
 
 // components
-import App from './components/app';
+import App from './app';
 
 // styles
-import './styles/Employee/list.scss';
-import './styles/main.scss';
-import './styles/Login/login.scss';
-import './styles/About/about.scss';
-import './styles/Common/header.scss';
-import './styles/Employee/details.scss';
+import '../styles/main.scss';
 
+import '../styles/elements/button.scss';
+import '../styles/elements/loader.scss';
+
+import '../styles/about/about.scss';
+import '../styles/login/login.scss';
+import '../styles/inventory/inventory.scss';
+
+import '../styles/employee/list.scss';
+import '../styles/employee/employee.scss';
+
+import '../styles/common/header.scss';
+import '../styles/common/footer.scss';
 
 const store = configureStore();
 
