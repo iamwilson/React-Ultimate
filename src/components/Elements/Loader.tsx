fc1229e323a0bddad222c0dc883fdf6b4b79ce3a
John Wilson <irewilson@gmail.com>
2018-08-07 10:34:46 +0800

    Update Loader.tsx

--- src/components/Elements/Loader.tsx
+++ src/components/Elements/Loader.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { connect } from "react-redux";
 
 interface ILoaderProps {
   isLoading: boolean;
@@ -10,14 +9,14 @@ class LoaderComponent extends React.Component<ILoaderProps> {
     super(props);
   }
 
+  componentDidMount() {}
+
   render() {
-
-    console.log(this.props.isLoading);
-
     if (this.props.isLoading) {
-      return <p>Loading ...</p>;
+      return <div className="loader-container">isLoading</div>;
+    } else{
+      return (<div></div>);
     }
-    return (null);
   }
 }
 
