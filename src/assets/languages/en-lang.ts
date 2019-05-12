74d4e3c3b92e1b4b8912f891001511d0046b385d
iamwilson <irewilson@gmail.com>
2019-05-13 00:18:11 +0800

    #fix - language resource exports

--- src/assets/languages/en-lang.ts
+++ src/assets/languages/en-lang.ts
@@ -2,45 +2,45 @@ export default {
     content: {
         language: {
             headerComponent: {
-                title: "Home"
+                title: 'Home'
             },
             footerComponent: {
-                language: "Language",
-                copyright: "Built with React 16.4.1 © Wilson",
+                language: 'Language',
+                copyright: 'Built with React 16.4.1 © Wilson',
             },
             sidebarComponent: {
-                department: "Departments",
-                about: "About",
-                logout: "Logout"
+                department: 'Departments',
+                about: 'About',
+                logout: 'Logout'
             },
             loginComponent: {
-                title: "EMS",
+                title: 'EMS',
                 labels: {
-                    username: "Username:",
-                    password: "Password:"
+                    username: 'Username:',
+                    password: 'Password:'
                 },
                 placeholders: {
-                    username: "Enter Username",
-                    password: "Enter Password"
+                    username: 'Enter Username',
+                    password: 'Enter Password'
                 },
                 errors: {
-                    username: "Please enter username",
-                    password: "Please enter password"
+                    username: 'Please enter username',
+                    password: 'Please enter password'
                 }
             },
             employeeComponent: {
-                title: "Employee List",
-                id: "Id",
-                name: "Name",
-                username: "Username",
-                email: "Email",
-                website: "Website",
-                action: "Action"
+                title: 'Employee List',
+                id: 'Id',
+                name: 'Name',
+                username: 'Username',
+                email: 'Email',
+                website: 'Website',
+                action: 'Action'
             },
             buttons: {
-                newEmployee: "New Employee",
-                delete: "Delete",
-                login: "Login"
+                newEmployee: 'New Employee',
+                delete: 'Delete',
+                login: 'Login'
             }
         }
     }
--- src/assets/languages/fr-lang.ts
+++ src/assets/languages/fr-lang.ts
@@ -2,45 +2,45 @@ export default {
     content: {
         language: {
             headerComponent: {
-                title: "Accueil"
+                title: 'Accueil'
             },
             footerComponent: {
-                language: "La langue",
-                copyright: "Construit avec React 16.4.1 © Wilson",
+                language: 'La langue',
+                copyright: 'Construit avec React 16.4.1 © Wilson',
             },
             sidebarComponent: {
-                department: "Départements",
-                about: "Environ",
-                logout: "Connectez - Out"
+                department: 'Départements',
+                about: 'Environ',
+                logout: 'Connectez - Out'
             },
             loginComponent: {
-                title: "EMS",
+                title: 'EMS',
                 labels: {
-                    username: "Nom d'utilisateur:",
-                    password: "Mot de passe:"
+                    username: 'Nom d\'utilisateur:',
+                    password: 'Mot de passe:'
                 },
                 placeholders: {
-                    username: "Votre nom d'utilisateur",
-                    password: "Entrer le mot de passe"
+                    username: 'Votre nom d\'utilisateur',
+                    password: 'Entrer le mot de passe'
                 },
                 errors: {
-                    username: "Veuillez entrer votre nom d'utilisateur",
-                    password: "Veuillez entrer le mot de passe"
+                    username: 'Veuillez entrer votre nom d\'utilisateur',
+                    password: 'Veuillez entrer le mot de passe'
                 }
             },
             employeeComponent: {
-                title: "Liste des employés",
-                id: "Id",
-                name: "Prénom",
-                username: "Nom d'utilisateur",
-                email: "Email",
-                website: "Site Internet",
-                action: "Action"
+                title: 'Liste des employés',
+                id: 'Id',
+                name: 'Prénom',
+                username: 'Nom d\'utilisateur',
+                email: 'Email',
+                website: 'Site Internet',
+                action: 'Action'
             },
             buttons: {
-                newEmployee: "Nouvel employé",
-                delete: "Effacer",
-                login: "S'identifier"
+                newEmployee: 'Nouvel employé',
+                delete: 'Effacer',
+                login: 'S\'identifier'
             }
         }
     }
